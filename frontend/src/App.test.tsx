df24ca857362ca251888942353c13a431ad5d1dc
Gus Taggart <gus.taggart17@gmail.com>
2026-04-12 18:41:44 -0400

    Finalize App tests

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -7,4 +7,8 @@ describe('The main app', () => {
         render(<App />);
         expect(screen.getByText('Orders')).toBeInTheDocument();
     });
+  it('should render the Payments link', () => {
+        render(<App />);
+        expect(screen.getByText('Payments')).toBeInTheDocument();
+    });
 });
